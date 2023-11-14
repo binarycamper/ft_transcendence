b5925c155439c36d12afe66aaaa59833cd80275f
Knosper <j.jesberger@yahoo.com>
2023-11-14 14:47:58 +0100

    add cookie-parser && main.ts overwork

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    transform: true, // automatically transform payloads to be objects typed according to their DTO classes
-    transformOptions: {
-      enableImplicitConversion: true,
-    },
-  }));
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	// Set up cookie parser middleware
+	app.use(cookieParser());
+
+	// Set up CORS configuration
+	app.enableCors({
+		origin: 'http://localhost:3000', //frontend's actual address? 3000 or 5173?
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,FETCH',
+		credentials: true,
+	});
+
+	// Set up global validation pipe
+	app.useGlobalPipes(new ValidationPipe({
+		whitelist: true,
+		forbidNonWhitelisted: true,
+		transform: true, // automatically transform payloads to be objects typed according to their DTO classes
+		transformOptions: {
+		enableImplicitConversion: true,
+		},
+	}));
+
+	await app.listen(3000);
 }
 bootstrap();
