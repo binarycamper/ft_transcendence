8fa47f1638f4b5f5689eb79f6f023785314b8c3b
Knosper <j.jesberger@yahoo.com>
2023-11-19 20:11:06 +0100

    start:dev backend

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -4,9 +4,9 @@ if   [ "$NODE_ENV" = 'development' ]; then
 	echo development
 	npm install
 	# development
-	npm run-script start
+	#npm run-script start
 	# watch mode
-	#npm run-script start:dev
+	npm run-script start:dev
 elif [ "$NODE_ENV" = 'production' ]; then
 	echo production
 	npm install
