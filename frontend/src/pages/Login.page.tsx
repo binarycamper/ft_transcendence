aba8963e9bf0667252ef915c40ac81f8a4d73688
Knosper <j.jesberger@yahoo.com>
2023-11-19 20:34:38 +0100

    Login button

--- frontend/src/pages/Login.page.tsx
+++ frontend/src/pages/Login.page.tsx
@@ -1,23 +1,23 @@
 export function Login() {
 	const handleLogin = async () => {
-	  try {
-		const response = await fetch('http://localhost:8080/auth/login');
-		if (!response.ok) {
-		  throw new Error('Login request failed');
+		try {
+			const response = await fetch('http://localhost:8080/auth/login');
+			if (!response.ok) {
+				throw new Error('Login request failed');
+			}
+			const data = await response.json();
+			console.log('Login Response:', data);
+			// You would typically redirect the user to the login URL here
+
+			window.location.href = data.url;
+		} catch (error) {
+			console.error('Failed to initiate login', error);
 		}
-		const data = await response.json();
-		console.log('Login Response:', data);
-		// You would typically redirect the user to the login URL here
-		
-		window.location.href = data.url;
-	  } catch (error) {
-		console.error('Failed to initiate login', error);
-	  }
 	};
-  
+
 	return (
-	  <div>
-		<button onClick={handleLogin}>Login</button>
-	  </div>
+		<div>
+			<button onClick={handleLogin}>Login</button>
+		</div>
 	);
-  }
+}
