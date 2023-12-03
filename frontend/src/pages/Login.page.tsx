2220a32d82fbe3c17ba0b3af321d6c61389bae04
Richard Meuth <rmeuth@student.42heilbronn.de>
2023-12-03 18:37:49 +0100

    update login page

--- frontend/src/pages/Login.page.tsx
+++ frontend/src/pages/Login.page.tsx
@@ -1,21 +1,46 @@
 import axios from 'axios';
+import {
+	Anchor,
+	Button,
+	Center,
+	Checkbox,
+	Paper,
+	PasswordInput,
+	Text,
+	TextInput,
+} from '@mantine/core';
+
+async function handleSignup() {
+	try {
+		//TODO: change to /auth/signup
+		const response = await axios.get('http://localhost:8080/auth/login', {
+			withCredentials: true,
+		});
+
+		window.location.href = response.data.url;
+	} catch (error) {
+		console.error('Failed to initiate signup', error);
+	}
+}
 
 export function Login() {
-	const handleLogin = async () => {
-		try {
-			const response = await axios.get('http://localhost:8080/auth/login', {
-				withCredentials: true,
-			});
-
-			window.location.href = response.data.url;
-		} catch (error) {
-			console.error('Failed to initiate login', error);
-		}
-	};
-
 	return (
-		<div>
-			<button onClick={handleLogin}>Login</button>
-		</div>
+		<Center my={200}>
+			<Paper className="login" radius={0} p={30}>
+				<Paper withBorder shadow="md" p={100} mt={30} mb={50} radius="md">
+					<TextInput label="Email address" placeholder="intra@student.42Heilbronn.de" size="md" />
+					<PasswordInput label="Password" placeholder="Your password" mt="md" size="md" />
+					<Checkbox label="Keep me logged in" mt="xl" size="md" />
+					<Button fullWidth mt="xl" size="md">
+						Login
+					</Button>
+				</Paper>
+				<Text ta="center" mt="md">
+					<Anchor<'a'> fw={700} onClick={handleSignup}>
+						Register
+					</Anchor>
+				</Text>
+			</Paper>
+		</Center>
 	);
 }
