1f492f172cdc59851e74063b46ba3a104ca2d583
Knosper <j.jesberger@yahoo.com>
2023-12-14 09:58:05 +0100

    add colours/styles to chatpage

--- frontend/src/pages/Chat.page.tsx
+++ frontend/src/pages/Chat.page.tsx
@@ -15,6 +15,7 @@ type ChatMessage = {
 export function Chat() {
 	const [messages, setMessages] = useState<ChatMessage[]>([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [pendingRequestCount, setPendingRequestCount] = useState(0);
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -29,14 +30,18 @@ export function Chat() {
 			});
 			if (!response.ok) {
 				navigate('/login');
+				return;
 			}
-			const data = await response.json();
-			console.log('TEST: ', data);
+			const data: ChatMessage[] = await response.json();
 			setMessages(data);
+
+			const pendingRequests = data.filter((message) => message.status === 'pending');
+			setPendingRequestCount(pendingRequests.length);
 		} catch (error) {
 			console.error('Error fetching messages:', error);
+		} finally {
+			setIsLoading(false);
 		}
-		setIsLoading(false);
 	};
 
 	const handleAction = async (messageId: string, action: string) => {
@@ -52,9 +57,48 @@ export function Chat() {
 		}
 	};
 
+	const pendingRequestsStyle: React.CSSProperties = {
+		backgroundColor: '#007bff', // A blue shade
+		color: 'white',
+		borderRadius: '50%',
+		padding: '0.5em',
+		marginLeft: '10px',
+		fontSize: '0.8em',
+		display: 'inline-block',
+		minWidth: '1.5em',
+		textAlign: 'center', // This should be fine now
+		lineHeight: '1.5em',
+	};
+
+	const buttonStyleBase: React.CSSProperties = {
+		border: 'none',
+		padding: '8px 16px',
+		borderRadius: '15px',
+		cursor: 'pointer',
+		fontWeight: 'bold',
+		textTransform: 'uppercase',
+		letterSpacing: '0.05em',
+		boxShadow: '0 2px 5px rgba(0,0,0,0.2)', // subtle shadow for depth
+		margin: '5px',
+		transition: 'all 0.3s ease', // smooth transition for hover effects
+	};
+
+	const acceptButtonStyle: React.CSSProperties = {
+		...buttonStyleBase,
+		backgroundColor: '#28a745', // Bootstrap green
+	};
+
+	const declineButtonStyle: React.CSSProperties = {
+		...buttonStyleBase,
+		backgroundColor: '#dc3545', // Bootstrap red
+	};
+
 	return (
 		<div>
-			<h1>Chat</h1>
+			<h1>
+				Open Requests:{' '}
+				{pendingRequestCount > 0 && <span style={pendingRequestsStyle}>{pendingRequestCount}</span>}
+			</h1>
 			{isLoading ? (
 				<p>Loading...</p>
 			) : (
@@ -69,8 +113,18 @@ export function Chat() {
 							)}
 							{message.messageType === 'friend_request' && (
 								<div>
-									<button onClick={() => handleAction(message.id, 'accept')}>Accept</button>
-									<button onClick={() => handleAction(message.id, 'decline')}>Decline</button>
+									<button
+										style={acceptButtonStyle}
+										onClick={() => handleAction(message.id, 'accept')}
+									>
+										Accept
+									</button>
+									<button
+										style={declineButtonStyle}
+										onClick={() => handleAction(message.id, 'decline')}
+									>
+										Decline
+									</button>
 								</div>
 							)}
 						</li>
