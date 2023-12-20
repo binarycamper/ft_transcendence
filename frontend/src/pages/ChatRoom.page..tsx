9bb4847879105c5d3728d44e791d5f37586a40b7
Knosper <j.jesberger@yahoo.com>
2023-12-20 01:37:52 +0100

    ChatRoom Layout && friends

--- frontend/src/pages/ChatRoom.page..tsx
+++ frontend/src/pages/ChatRoom.page..tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { SocketContext } from './context/socketContext';
 import { useSearchParams } from 'react-router-dom';
 import sanitizeHtml from 'sanitize-html';
@@ -7,6 +7,24 @@ type ChatMessage = {
 	id: string;
 	content: string;
 	senderId: string;
+};
+
+type Friend = {
+	id: string;
+	name: string;
+	// Add other relevant friend properties
+};
+
+const friendsListStyle = {
+	listStyleType: 'none',
+	padding: 0,
+	margin: 0,
+};
+
+const friendItemStyle = {
+	cursor: 'pointer',
+	padding: '10px',
+	borderBottom: '1px solid #ccc',
 };
 
 // Styles for the button
@@ -19,11 +37,12 @@ const buttonStyle = {
 };
 
 export const ChatRoom = () => {
-	const [searchParams] = useSearchParams();
+	const [searchParams, setSearchParams] = useSearchParams();
 	const friendId = searchParams.get('friendId');
 	const [messages, setMessages] = useState<ChatMessage[]>([]);
 	const socket = useContext(SocketContext);
 	const [inputValue, setInputValue] = useState('');
+	const [friends, setFriends] = useState<Friend[]>([]);
 	const [chat, setChat] = useState('');
 
 	// Listen for messages from the server
@@ -32,6 +51,27 @@ export const ChatRoom = () => {
 		setMessages((prevMessages) => [...prevMessages, message]);
 		setChat(chat + message.content + '\n'); // Store the chat history
 	});
+
+	useEffect(() => {
+		// Fetch friends list when component mounts
+		const fetchFriends = async () => {
+			try {
+				const response = await fetch('http://localhost:8080/user/friends', {
+					credentials: 'include',
+				});
+				if (!response.ok) {
+					throw new Error('Failed to fetch friends');
+				}
+				const friendsList = await response.json();
+				console.log('GOOD: ', friendsList);
+				setFriends(friendsList);
+			} catch (error) {
+				console.error('Error fetching friends:', error);
+			}
+		};
+
+		fetchFriends();
+	}, []);
 
 	// Clear the input field after sending the message
 	const handleSendMessage = () => {
@@ -48,7 +88,7 @@ export const ChatRoom = () => {
 
 	const deleteMyChats = async () => {
 		try {
-			const response = await fetch('/chat/myChats', {
+			const response = await fetch('http://localhost:8080/chat/myChats', {
 				method: 'DELETE',
 				credentials: 'include',
 			});
@@ -65,34 +105,62 @@ export const ChatRoom = () => {
 		}
 	};
 
+	const switchChat = (friendId: string) => {
+		setSearchParams({ friendId }); // This will change the URL parameter
+		// Additional logic to switch chat context can be added here
+	};
+
 	return (
-		<div style={{ display: 'flex', flexDirection: 'column' }}>
-			<h2>Chat Room</h2>
-			<p>Chatting with friend ID: {friendId}</p>
-			<button onClick={deleteMyChats} style={buttonStyle}>
-				Clear All My Chats
-			</button>
-			<div style={{ flexGrow: 1, minHeight: 200 }}>
-				<textarea
-					placeholder="Chat Verlauf"
-					value={chat}
-					readOnly={true}
-					onChange={(event) => setChat(event.target.value)}
-				></textarea>
-				<ul>
-					{messages.map((message) => (
-						<li key={message.id}>{message.content}</li>
-					))}
-				</ul>
+		<div style={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+			{/* Friends List */}
+			<div style={{ marginBottom: '20px' }}>
+				{' '}
+				{/* Add margin for spacing */}
+				<h3>My Friends</h3>
+				{friends.length > 0 ? (
+					<ul style={friendsListStyle}>
+						{friends.map((friend) => (
+							<li key={friend.id} style={friendItemStyle} onClick={() => switchChat(friend.id)}>
+								{friend.name}
+							</li>
+						))}
+					</ul>
+				) : (
+					<p>No friends found.</p>
+				)}
 			</div>
-			<div>
-				<input
-					type="text"
-					placeholder="Send message"
-					value={inputValue}
-					onChange={(event) => setInputValue(event.target.value)}
-				/>
-				<button onClick={handleSendMessage}>Send</button>
+
+			{/* Chat Room */}
+			<div style={{ width: '100%' }}>
+				{' '}
+				{/* Ensure this div takes full width */}
+				<h2>Chat Room</h2>
+				<p>Chatting with friend ID: {friendId}</p>
+				<button onClick={deleteMyChats} style={buttonStyle}>
+					Clear All My Chats
+				</button>
+				<div style={{ flexGrow: 1, minHeight: '200px' }}>
+					<textarea
+						placeholder="Chat Verlauf"
+						value={chat}
+						readOnly={true}
+						onChange={(event) => setChat(event.target.value)}
+					></textarea>
+					<ul>
+						{messages.map((message) => (
+							<li key={message.id}>{message.content}</li>
+						))}
+					</ul>
+				</div>
+				<div>
+					<input
+						type="text"
+						placeholder="Send message"
+						value={inputValue}
+						onChange={(event) => setInputValue(event.target.value)}
+					/>
+					<button onClick={handleSendMessage}>Send</button>
+				</div>
 			</div>
 		</div>
 	);
