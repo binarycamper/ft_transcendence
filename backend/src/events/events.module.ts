ed212851026db111dc4b03c7aa744904f58912f8
PoroCoin <PoroCoin@users.noreply.github.com>
2023-12-28 21:42:38 +0100

    fixed import

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -5,12 +5,10 @@ import { EventsService } from './events.service';
 import { UserModule } from '../user/user.module'; // Import the UserModule if needed
 import { JwtModule } from '@nestjs/jwt';
 import { ChatModule } from 'src/chat/chat.module';
-import { ChatService } from 'src/chat/chat.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatMessage } from 'src/chat/chat.entity';
 import { Match } from 'src/matchmaking/matchmaking.entity';
 import { MatchmakingModule } from 'src/matchmaking/matchmaking.module';
-import { EventService } from './event.service';
 
 @Module({
 	imports: [
@@ -23,7 +21,7 @@ import { EventService } from './event.service';
 			signOptions: { expiresIn: '1d' }, // Set an appropriate expiration time for the tokens
 		}),
 	],
-	providers: [EventsGateway, EventsService, EventService],
-	exports: [EventsGateway, EventsService, EventService],
+	providers: [EventsGateway, EventsService],
+	exports: [EventsGateway, EventsService],
 })
 export class EventsModule {}
