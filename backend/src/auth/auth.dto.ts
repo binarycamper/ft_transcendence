66ba2d7ad7c006da1b4d33fb1fc61a64a0c0c2aa
PoroCoin <PoroCoin@users.noreply.github.com>
2024-01-09 16:50:36 +0100

    fix length for OAuth code

--- backend/src/auth/auth.dto.ts
+++ backend/src/auth/auth.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString, Length, IsAlphanumeric } from 'class-validator';
 export class AuthCallbackDto {
 	@IsNotEmpty({ message: 'Code is required.' })
 	@IsString({ message: 'Code must be a string.' })
-	@Length(10, 100, { message: 'Code must be between 10 and 100 characters.' }) // Beispielwerte für min und max
+	@Length(287, 287, { message: 'Code must be between 10 and 100 characters.' }) // Beispielwerte für min und max
 	@IsAlphanumeric()
 	code: string;
 }
