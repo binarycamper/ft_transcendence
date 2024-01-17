293f09f36a52d205af5025369415238796da30fb
Knosper <j.jesberger@yahoo.com>
2024-01-17 13:10:33 +0100

    small change

--- frontend/src/pages/Game.page.tsx
+++ frontend/src/pages/Game.page.tsx
@@ -2,45 +2,9 @@ import { useState, useEffect, useRef } from 'react';
 import { socket } from '../services/socket';
 
 const GamePage = () => {
-	// Screen dimensions
-	const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-	const [screenHeight, setScreenHeight] = useState(window.innerHeight);
-
-	// State for user and game
 	const [currentUserId, setCurrentUserId] = useState(null);
 	const [currentUserName, setCurrentUserName] = useState(null);
-	const [ballPosition, setBallPosition] = useState({ x: screenWidth / 2, y: screenHeight / 2 });
-	const [playerScores, setPlayerScores] = useState({ player1: 0, player2: 0 });
 	const [gameReady, setGameReady] = useState(false);
-
-	// State for paddle movement
-	const [leftPaddleY, setLeftPaddleY] = useState(0);
-	const [rightPaddleY, setRightPaddleY] = useState(0);
-
-	useEffect(
-		() => {
-			const handleResize = () => {
-				setScreenWidth(window.innerWidth);
-				setScreenHeight(window.innerHeight);
-
-				// calculate the ratio of the ball's current position to the total width and height of the game area
-				//const ballXRatio = ballPos.x / 1280;
-				//const ballYRatio = ballPos.y / 720;
-
-				// multiply the new dimensions of the game area by these ratios to get the new position of the ball
-				//setBallPos({ x: ballXRatio * window.innerWidth, y: ballYRatio * window.innerHeight });
-			};
-
-			window.addEventListener('resize', handleResize);
-
-			return () => {
-				window.removeEventListener('resize', handleResize);
-			};
-		},
-		[
-			/*ballPos*/
-		],
-	);
 
 	useEffect(() => {
 		async function getCurrentUserId() {
@@ -63,13 +27,13 @@ const GamePage = () => {
 					credentials: 'include',
 				});
 				if (!response.ok) {
-					//window.location.href = 'http://localhost:5173/';
+					window.location.href = 'http://localhost:5173/';
 					return;
 				}
 				const data = await response.json();
 				//TODO: Set ur data here you can have all infos of game entity. const game.playerOneScore = data.playerOneScore etc...
 			} catch (error) {
-				//window.location.href = 'http://localhost:5173/';
+				window.location.href = 'http://localhost:5173/';
 			}
 		}
 
